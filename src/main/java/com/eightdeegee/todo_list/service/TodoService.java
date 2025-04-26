664066fd47c3d790ba9e7d5cd85c85b3cf37a53d
8DeeGee <8deegee@gmail.com>
2025-04-25 21:18:05 -0300

    Changes to be committed: new file:   src/main/java/com/eightdeegee/todo_list/controller/TodoController.java modified:   src/main/java/com/eightdeegee/todo_list/service/TodoService.java

--- src/main/java/com/eightdeegee/todo_list/service/TodoService.java
+++ src/main/java/com/eightdeegee/todo_list/service/TodoService.java
@@ -1,10 +1,43 @@
 package com.eightdeegee.todo_list.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import com.eightdeegee.todo_list.entity.Todo;
+import com.eightdeegee.todo_list.repository.TodoRepository;
 
 @Service
 public class TodoService {
 
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
     
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by("priority").descending().and(
+            Sort.by("name").ascending()
+        );
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
+    }
     
 }
